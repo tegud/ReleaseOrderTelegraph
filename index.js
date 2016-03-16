16a9119558b49b716212d2978239440918aeab34
Steve Elliott <steve@tegud.net>
2016-03-16 10:45:50 +0000

    Stupid mistake

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-require('fs').readdirSync(__dirname, (err, files) => {
-    console.log('listing files in directory');
-    console.log(files);
-});
-
-//require('./lib/server')({ port: 1234 }).start();
-function ping() {
-    console.log('PING');
-    setTimeout(ping, 60000);
-}
-
-ping();
+require('./lib/server')({ port: 1234 }).start();
