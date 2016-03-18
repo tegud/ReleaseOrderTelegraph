70a886c82dae8d76cd73b23073952ab6ce11cd5c
Steve Elliott <steve@tegud.net>
2016-03-18 15:56:49 +0000

    Refacotr

--- tests/checks/schedule.js
+++ tests/checks/schedule.js
@@ -112,39 +112,130 @@ describe('schedule', () => {
             .should.eventually.eql({ signal: 'amber', reason: 'Reduced support during lunch' });
     });
 
-    it('sets reason to default reason for unmatched schedule, matching signal', () => {
-        fakeCurrentDate = '2016-03-14T00:00:00';
+    describe('defaultReasons', () => {
+        it('sets reason to default reason for unmatched schedule, matching signal', () => {
+            fakeCurrentDate = '2016-03-14T00:00:00';
 
-        const schedule = scheduleCheck({
-            defaultReasons: {
-                'red': 'Cannot release outside of permitted schedule.'
-            },
-            schedule: {
-                'Monday': [
-                    { from: '09:00', to: '16:00' }
-                ]
-            }
+            const schedule = scheduleCheck({
+                defaultReasons: {
+                    'red': 'Cannot release outside of permitted schedule.'
+                },
+                schedule: {
+                    'Monday': [
+                        { from: '09:00', to: '16:00' }
+                    ]
+                }
+            });
+
+            return schedule.start().then(() => schedule.getState())
+                .should.eventually.eql({ signal: 'red', reason: 'Cannot release outside of permitted schedule.' });
         });
 
-        return schedule.start().then(() => schedule.getState())
-            .should.eventually.eql({ signal: 'red', reason: 'Cannot release outside of permitted schedule.' });
+        it('sets reason to default reason for matching schedule and matching signal', () => {
+            fakeCurrentDate = '2016-03-14T09:00:00';
+
+            const schedule = scheduleCheck({
+                defaultReasons: {
+                    'green': 'Ok to release during scheduled hours'
+                },
+                schedule: {
+                    'Monday': [
+                        { from: '09:00', to: '16:00' }
+                    ]
+                }
+            });
+
+            return schedule.start().then(() => schedule.getState())
+                .should.eventually.eql({ signal: 'green', reason: 'Ok to release during scheduled hours' });
+        });
     });
 
-    it('sets reason to default reason for matching schedule and matching signal', () => {
-        fakeCurrentDate = '2016-03-14T09:00:00';
+    describe('overrides', () => {
+        it('matching overrides are applied', () => {
+            fakeCurrentDate = '2016-03-14T13:00:00';
 
-        const schedule = scheduleCheck({
-            defaultReasons: {
-                'green': 'Ok to release during scheduled hours'
-            },
-            schedule: {
-                'Monday': [
-                    { from: '09:00', to: '16:00' }
+            const schedule = scheduleCheck({
+                schedule: {
+                    'Monday': [
+                        { from: '09:00', to: '16:00' }
+                    ]
+                },
+                overrides: [
+                    { from: '2016-03-14T12:00:00', to: '2016-03-14T16:00:00', signal: 'red', 'reason': 'Change Freeze for easter period' }
                 ]
-            }
+            });
+
+            return schedule.start().then(() => schedule.getState())
+                .should.eventually.eql({ signal: 'red', reason: 'Change Freeze for easter period' });
         });
 
-        return schedule.start().then(() => schedule.getState())
-            .should.eventually.eql({ signal: 'green', reason: 'Ok to release during scheduled hours' });
+        it('start of matching override is applied', () => {
+            fakeCurrentDate = '2016-03-14T12:00:00';
+
+            const schedule = scheduleCheck({
+                schedule: {
+                    'Monday': [
+                        { from: '09:00', to: '16:00' }
+                    ]
+                },
+                overrides: [
+                    { from: '2016-03-14T12:00:00', to: '2016-03-14T16:00:00', signal: 'red', 'reason': 'Change Freeze for easter period' }
+                ]
+            });
+
+            return schedule.start().then(() => schedule.getState())
+                .should.eventually.eql({ signal: 'red', reason: 'Change Freeze for easter period' });
+        });
+
+        it('override applies outside of matching schedule', () => {
+            fakeCurrentDate = '2016-03-14T12:00:00';
+
+            const schedule = scheduleCheck({
+                schedule: { },
+                overrides: [
+                    { from: '2016-03-14T12:00:00', to: '2016-03-14T16:00:00', signal: 'red', 'reason': 'Change Freeze for easter period' }
+                ]
+            });
+
+            return schedule.start().then(() => schedule.getState())
+                .should.eventually.eql({ signal: 'red', reason: 'Change Freeze for easter period' });
+        });
+
+        it('non-matching override is not applied', () => {
+            fakeCurrentDate = '2016-03-14T11:59:59';
+
+            const schedule = scheduleCheck({
+                schedule: {
+                    'Monday': [
+                        { from: '09:00', to: '16:00' }
+                    ]
+                },
+                overrides: [
+                    { from: '2016-03-14T12:00:00', to: '2016-03-14T16:00:00', signal: 'red', 'reason': 'Change Freeze for easter period' }
+                ]
+            });
+
+            return schedule.start().then(() => schedule.getState())
+                .should.eventually.eql({ signal: 'green' });
+        });
+
+        it('applies the last of multiple matching overrides', () => {
+            fakeCurrentDate = '2016-03-14T13:00:00';
+
+            const schedule = scheduleCheck({
+                schedule: {
+                    'Monday': [
+                        { from: '09:00', to: '16:00' }
+                    ]
+                },
+                overrides: [
+                    { from: '2016-03-14T12:00:00', to: '2016-03-14T16:00:00', signal: 'red', 'reason': 'Change Freeze for easter period' },
+                    { from: '2016-03-14T13:00:00', to: '2016-03-14T14:00:00', signal: 'red', 'reason': 'Extra special change Freeze for easter period' }
+                ]
+            });
+
+            return schedule.start().then(() => schedule.getState())
+                .should.eventually.eql({ signal: 'red', reason: 'Extra special change Freeze for easter period' });
+        });
     });
 });
