9bfaf5fdf17ef6096d051f3797daa4e48f62852f
Steve Elliott <steve@tegud.net>
2016-04-04 22:00:02 +0100

    Just run one failing test for now

--- tests/checks/schedule.js
+++ tests/checks/schedule.js
@@ -26,7 +26,7 @@ describe('schedule', () => {
         fakeMoment.clear();
     });
 
-    it('should return red when current date is outside the configured schedule', () =>
+    it.only('should return red when current date is outside the configured schedule', () =>
         createScheduleCheck()
             .then(getState())
             .then(result => new Promise(resolve => resolve(result.signal)))
